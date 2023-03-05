8ac661fb14a9c38bb97e7b554ce1f7c4107d76ba
Daniel Leech <dan.t.leech@gmail.com>
2023-03-05 10:15:49 +0000

    Adding tui infra

--- src/ui/app.rs
+++ src/ui/app.rs
@@ -1,7 +1,27 @@
-use std::io;
+use std::{io, time::Duration};
 
-use tui::{Terminal, backend::CrosstermBackend};
+use crossterm::event::{self, poll, Event};
+use tui::{
+    backend::{Backend, CrosstermBackend},
+    Frame, Terminal,
+};
 
-pub struct App<'a> {
-    terminal: &'a mut Terminal<CrosstermBackend<io::Stdout>>
+pub struct App {}
+
+impl App {
+    pub async fn run<'a>(
+        &self,
+        terminal: &'a mut Terminal<CrosstermBackend<io::Stdout>>,
+    ) -> Result<(), anyhow::Error> {
+        loop {
+            terminal.draw(|f| { self.draw(f).expect("Could not draw frame"); })?;
+            if (poll(Duration::from_millis(1000)))? {
+                if let Event::Key(_key) = event::read()? {}
+            }
+        }
+    }
+
+    fn draw<B: Backend>(&self, f: &mut Frame<B>) -> Result<(), anyhow::Error> {
+        todo!()
+    }
 }
