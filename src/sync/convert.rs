6047955cf6786016069af1a84d6d3a714fca92a4
Daniel <daniel@dantleech.com>
2023-08-05 12:58:41 +0100

    covert

--- src/sync/convert.rs
+++ src/sync/convert.rs
@@ -1,5 +1,7 @@
-use diesel::prelude::*;
-use diesel::SqliteConnection;
+
+use libsqlite3_sys::sqlite3_expired;
+use sqlx::QueryBuilder;
+use sqlx::SqliteConnection;
 
 use crate::client;
 use crate::event::input::EventSender;
@@ -7,8 +9,6 @@ use crate::event::input::InputEvent;
 use crate::event::logger::Logger;
 use crate::store::activity::Activity;
 use crate::store::activity::ActivitySplit;
-use crate::store::activity::RawActivity;
-use crate::store::schema;
 
 pub struct AcitivityConverter<'a> {
     connection: &'a mut SqliteConnection,
@@ -29,12 +29,11 @@ impl AcitivityConverter<'_> {
         }
     }
     pub async fn convert(&mut self) -> Result<(), anyhow::Error> {
-        use crate::store::schema::raw_activity;
-
-        let raw_activities: Vec<RawActivity> = raw_activity::table
-            .select(RawActivity::as_select())
-            .filter(raw_activity::synced.eq(false))
-            .load(self.connection)?;
+        let raw_activities = sqlx::query!(
+            r#"
+            SELECT activity, listed FROM raw_activity WHERE synced = false
+            "#
+        ).fetch_all(self.connection).await?;
 
         for raw_activity in raw_activities {
             let listed: client::Activity =
@@ -65,20 +64,64 @@ impl AcitivityConverter<'_> {
                 athletes: listed.athlete_count,
             };
 
-            diesel::insert_into(schema::activity::table)
-                .values(&activity)
-                .on_conflict(schema::activity::id)
-                .do_nothing()
-                .execute(self.connection)?;
+            sqlx::query!(
+                r#"
+                INSERT INTO activity (
+                    id,
+                    title,
+                    description,
+                    activity_type,
+                    distance,
+                    moving_time,
+                    elapsed_time,
+                    total_elevation_gain,
+                    sport_type,
+                    average_heartrate,
+                    max_heartrate,
+                    start_date,
+                    summary_polyline,
+                    average_cadence,
+                    kudos,
+                    location_country,
+                    location_state,
+                    location_city,
+                    athletes
+                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                ON CONFLICT DO NOTHING
+                "#,
+                listed.id,
+                listed.name,
+                match listed.description {
+                    Some(d) => d,
+                    None => "".to_string(),
+                },
+                listed.sport_type.clone(),
+                listed.distance,
+                listed.moving_time,
+                listed.elapsed_time,
+                listed.total_elevation_gain,
+                listed.sport_type.clone(),
+                listed.average_heartrate,
+                listed.max_heartrate,
+                listed.start_date.map(|date| date.naive_utc()),
+                Some(listed.map.summary_polyline),
+                listed.average_cadence,
+                listed.kudos_count,
+                listed.location_country,
+                listed.location_state,
+                listed.location_city,
+                listed.athlete_count,
+            ).execute(self.connection).await;
 
             if let Some(full_activity) = raw_activity.activity {
                 let activity: client::Activity =
                     serde_json::from_str(full_activity.as_str()).expect("Could not decode JSON");
-                diesel::delete(
-                    schema::activity_split::table
-                        .filter(schema::activity_split::activity_id.eq(activity.id)),
-                )
-                .execute(self.connection)?;
+                sqlx::query!(
+                    r#"
+                    DELETE FROM activity_split WHERE activity_id = ?
+                    "#,
+                    activity.id
+                ).execute(self.connection).await?;
 
                 if let Some(laps) = activity.splits_standard {
                     let mut activity_laps: Vec<ActivitySplit> = vec![];
@@ -93,10 +136,21 @@ impl AcitivityConverter<'_> {
                             split: lap.split,
                         });
                     }
-
-                    diesel::insert_into(schema::activity_split::table)
-                        .values(&activity_laps)
-                        .execute(self.connection)?;
+                    let qb = QueryBuilder::new(
+                        r#"
+                        INSERT INTO activity_lap (activity_id, distance, moving_time, elapsed-time, average_speed, elevation_difference, split)")
+                        "#
+                    );
+                    qb.push_values(activity_laps, |mut b, activity_lap| {
+                        b.push_bind(activity_lap.id);
+                        b.push_bind(activity_lap.distance);
+                        b.push_bind(activity_lap.moving_time);
+                        b.push_bind(activity_lap.elapsed_time);
+                        b.push_bind(activity_lap.average_speed);
+                        b.push_bind(activity_lap.elevation_difference);
+                        b.push_bind(activity_lap.split);
+                    });
+                    qb.build().execute(self.connection).await?;
                 }
             }
         }
